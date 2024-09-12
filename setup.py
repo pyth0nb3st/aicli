b67a71ea2e40d49a93c0b204c4677d9abc87d3cd
callmexss <callmexss@126.com>
2024-09-12 21:07:19 +0800

    feat: env before dot env

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="aicli-agent",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
